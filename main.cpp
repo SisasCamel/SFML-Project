462145f35bcd1c7330250fc4bc80d0ec01b9fe08
SisasCamel <gsanchezv07@outlook.es>
2021-04-18 16:53:41 -0500

    no more rec

--- main.cpp
+++ main.cpp
@@ -5,14 +5,7 @@ int main()
     //settings for antialiasing
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
-
-
     sf::RenderWindow window(sf::VideoMode(250, 250), "SFML works!", sf::Style::Default, settings);
-    sf::RectangleShape rec(sf::Vector2f(0.0f, 0.0f));
-    rec.setPosition(sf::Vector2f(250.0f, 33.0f));
-    rec.setSize(sf::Vector2f(200.0f, 100.0f));
-    rec.setOutlineThickness(10.0f);
-    rec.setOutlineColor(sf::Color(250, 13, 44));
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
@@ -27,7 +20,6 @@ int main()
 
         window.clear();
         window.draw(shape);
-        window.draw(rec);
         window.display();
     }
 
